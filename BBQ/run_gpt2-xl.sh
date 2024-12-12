40ab0bfb0d57e47d0db05b816ab4f0e3f41751a3
cczeng <cczeng@umich.edu>
2024-12-12 10:49:11 -0500

    new sh files

---
+++ BBQ/run_gpt2-xl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Convert prompt format
+python3 data/convert_format.py
+python3 data/convert_format.py --few_shot
+
+# Test 1 - Gender Plain Neg Without Debiasing
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_lower.jsonl --debias_prompt gender-plain-neg_without
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_upper.jsonl --debias_prompt gender-plain-neg_without
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_lower.jsonl --debias_prompt gender-plain-neg_without
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_upper.jsonl --debias_prompt gender-plain-neg_without
+
+# Test 2 - Gender Plain Neg With Debiasing
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_lower.jsonl --debias_prompt gender-plain-neg_with
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_upper.jsonl --debias_prompt gender-plain-neg_with
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_lower.jsonl --debias_prompt gender-plain-neg_with
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_upper.jsonl --debias_prompt gender-plain-neg_with
+
+# Test 3 - Gender Instruct Neg Without Debiasing
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_lower.jsonl --debias_prompt gender-instruct-neg_without
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_upper.jsonl --debias_prompt gender-instruct-neg_without
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_lower.jsonl --debias_prompt gender-instruct-neg_without
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_upper.jsonl --debias_prompt gender-instruct-neg_without
+
+# Test 4 - Gender Instruct Neg With Debiasing
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_lower.jsonl --debias_prompt gender-instruct-neg_with
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_1_upper.jsonl --debias_prompt gender-instruct-neg_with
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_lower.jsonl --debias_prompt gender-instruct-neg_with
+python3 src/pred.py --model facebook/opt-1.3b --file data/jsonl/eval_prompt_fewshot_1_upper.jsonl --debias_prompt gender-instruct-neg_with
+
+# Results
+python3 evaluation/eval_bbq.py --result_dir result
